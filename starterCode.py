d986494b617d1cb0d37e30311e6bc247e462685f
DavidNovikov <dn9678@gmail.com>
2022-11-03 14:37:47 -0400

    starter code and presentation touch up

--- starterCode.py
+++ starterCode.py
@@ -3,6 +3,9 @@ import torch
 
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
+red = (0, 0, 255)
+green = (0, 255, 0)
+
 def drawboxWithNameAndConf(result, image):
     h, w, _ = image.shape
     x1 = int(result.xmin*w)
@@ -16,17 +19,8 @@ def drawboxWithNameAndConf(result, image):
 def watchVideo():
     video = cv2.VideoCapture('intersection.mp4')
     ret, frame = video.read()
-    
-    w  = video.get(cv2.CAP_PROP_FRAME_WIDTH)   # float `width`
-    h = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    
-    slope1 = (100 - 200) / 1280
-    slope2 = (450 - 600) / 1280
-    intercept1 = 200
-    intercept2 = 600
 
     while ret:
-        dangerous = False
         
         frameForModel = cv2.cvtColor(
             frame, cv2.COLOR_BGR2RGB) 
@@ -35,34 +29,9 @@ def watchVideo():
         for _, r in results.pandas().xyxyn[0].iterrows():
             if r.values[6] == 'person':
                 drawboxWithNameAndConf(r, frame)
-                
-                x1 = r.xmin*w
-                y1 = r.ymin*h
-                x2 = r.xmax*w
-                y2 = r.ymax*h
-                
-                x = (x1+x2)/2
-                y = (y1+y2)/2
-                
-                if x * slope1 + intercept1 < y and x * slope2 + intercept2 > y:
-                    dangerous = True
-                    cv2.circle(frame, (int(x), int(y)), 10, (0, 0, 255), 13)
-
-        color = None
-        text = ''
-        if dangerous:
-            color = (0, 0, 255)
-            text = 'DANGER'
-        else:
-            color = (0, 255, 0)
-            text = 'SAFE'
-            
-        cv2.putText(frame, text, (600, 300), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
-        cv2.line(frame, (0, 200), (1280, 100), color, 2)
-        cv2.line(frame, (0, 600), (1280, 450), color, 2)
         
         cv2.imshow("Video", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(35) & 0xFF == ord('q'):
             break
 
         ret, frame = video.read()

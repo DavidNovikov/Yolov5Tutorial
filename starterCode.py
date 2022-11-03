57b93af78acf0b4f88b136c39d35a5d34357129c
DavidNovikov <dn9678@gmail.com>
2022-11-03 14:40:34 -0400

    starter code and presentation touch up/Users/davidnovikov/miniconda3/envs/testenv/bin/python

--- starterCode.py
+++ starterCode.py
@@ -7,33 +7,35 @@ red = (0, 0, 255)
 green = (0, 255, 0)
 
 def drawboxWithNameAndConf(result, image):
-    h, w, _ = image.shape
-    x1 = int(result.xmin*w)
+    h, w, _ = image.shape # dimensions of image
+    
+    x1 = int(result.xmin*w) #dimensions of object detected
     y1 = int(result.ymin*h)
     x2 = int(result.xmax*w)
     y2 = int(result.ymax*h)
-    cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
+    
+    cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2) # draw bounding box
     cv2.putText(image, f'{result.values[6]} {result.confidence:.2f}',
-                (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+                (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2) # write confidence
 
 def watchVideo():
-    video = cv2.VideoCapture('intersection.mp4')
+    video = cv2.VideoCapture('intersection.mp4') # oepn and read video
     ret, frame = video.read()
 
     while ret:
         
         frameForModel = cv2.cvtColor(
-            frame, cv2.COLOR_BGR2RGB) 
-        results = model(frameForModel)
-
-        for _, r in results.pandas().xyxyn[0].iterrows():
-            if r.values[6] == 'person':
-                drawboxWithNameAndConf(r, frame)
+            frame, cv2.COLOR_BGR2RGB) # preprocessing
         
-        cv2.imshow("Video", frame)
+        results = model(frameForModel) # inference 
+
+        for _, r in results.pandas().xyxyn[0].iterrows(): # parse results
+            drawboxWithNameAndConf(r, frame)
+        
+        cv2.imshow("Video", frame) # show video
         if cv2.waitKey(35) & 0xFF == ord('q'):
             break
 
-        ret, frame = video.read()
+        ret, frame = video.read() # read next frame
         
 watchVideo()
